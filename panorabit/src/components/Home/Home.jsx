59c863255ece1ccf909eded875ae863b0578e81c
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2023-05-21 02:11:33 +0530

    Add initial dependencies
    axios,react router dom, bootstrap, sass

---
+++ panorabit/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.scss'
+
+export default function Home() {
+    return (
+        <div className="container-md home-page">
+            <h1>Hello World</h1>
+        </div>
+    )
+}
