905d1c48f4af010c54e1b7c3553da2439db19cb2
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2023-05-22 01:18:35 +0530

    Refactor removed wave flag for mob view

--- panorabit/src/assets/svgs/wave/Wave.jsx
+++ panorabit/src/assets/svgs/wave/Wave.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import './Wave.scss'
 import WaveGradientWeb from './webwave/WaveGradientWeb'
 import WaveShadowWeb from './webwave/WaveShadowWeb'
-import WaveShadowMob from './mobwave/WaveShadowMob'
-import WaveGradientMob from './mobwave/WaveGradientMob'
 
 export default function Wave() {
     return (
@@ -11,10 +8,6 @@ export default function Wave() {
             <div className="wave-web">
                 <WaveShadowWeb />
                 <WaveGradientWeb />
-            </div>
-            <div className="wave-mob">
-                <WaveShadowMob />
-                <WaveGradientMob />
             </div>
         </div>
     )
