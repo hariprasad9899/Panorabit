aebe0dd7643d23bc9c341133246bef2741ffffc6
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2023-05-22 00:52:39 +0530

    Revert "Add wave svg url for web"
    This reverts commit ffdc21439414eaf366b413c93b02cb98a6cab45a.

--- panorabit/src/components/Home/Intro/Intro.jsx
+++ panorabit/src/components/Home/Intro/Intro.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Intro.scss'
 import Accounts from '../Accounts/Accounts'
-import Wave from '../../../assets/svgs/wave/Wave'
+import Wave from '../../../assets/svgs/wave'
 
 export default function Intro() {
     return (
-        <main className="container-fluid p-0 m-0 landing-page">
+        <main className="container-fluid p-0 m-0 border landing-page">
             <Wave />
         </main>
     )
