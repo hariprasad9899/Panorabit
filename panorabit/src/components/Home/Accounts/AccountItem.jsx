bc04b10ebcde62c227b9bbcc364a2e0da7f23051
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2023-05-22 14:16:01 +0530

    Add sroll element to display list of accounts

--- panorabit/src/components/Home/Accounts/AccountItem.jsx
+++ panorabit/src/components/Home/Accounts/AccountItem.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 export default function AccountItem({ profilepicture, username }) {
     return (
-        <div className="row border-bottom py-1 border-bottom-primary">
-            <div className="col-auto bg align-self-center">
+        <div className="row py-1 align-items-center border-bottom border-bottom-primary">
+            <div className="col-auto p-0">
                 <img src={profilepicture} className="acc-profile-img" />
             </div>
-            <div className="col-10 p-0">
-                <p className="acc-profile-username">{username}</p>
-            </div>
+            <span className="col-10 p-2 acc-profile-username">{username}</span>
         </div>
     )
 }
--- panorabit/src/components/Home/Accounts/Accounts.jsx
+++ panorabit/src/components/Home/Accounts/Accounts.jsx
@@ -4,12 +4,14 @@ import AccountList from './AccountList'
 
 export default function Accounts() {
     return (
-        <div className="container-md position-absolute shadow-lg top-50 start-50 translate-middle centered-div">
+        <div className="container-md pb-3 position-absolute shadow-lg top-50 start-50 translate-middle centered-div">
             <div className="row account-select p-4 text-center">
                 <p className="h5">Select an account</p>
             </div>
-            <div className="row px-4 pt-1 justify-content-center account-list">
-                <AccountList />
+            <div className="row account-list-wrapper px-3 pe-1">
+                <div className="pt-1 justify-content-center account-list">
+                    <AccountList />
+                </div>
             </div>
         </div>
     )
