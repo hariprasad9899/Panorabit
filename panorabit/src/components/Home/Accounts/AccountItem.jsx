54b02442cc964513819194c513056f9a5fb159e8
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2023-05-25 02:20:23 +0530

    Update paddings and responsiveness

--- panorabit/src/components/Home/Accounts/AccountItem.jsx
+++ panorabit/src/components/Home/Accounts/AccountItem.jsx
@@ -15,12 +15,41 @@ export default function AccountItem({
             onClick={() => handleClick(id)}
             to={`profile/${id}`}
             style={{ textDecoration: 'none', color: 'black' }}
-            className="row py-1 align-items-center border-bottom border-bottom-primary account-item"
+            className="row align-items-center border-bottom border-bottom-primary account-item"
         >
-            <div className="col-auto p-0">
-                <img src={profilepicture} className="acc-profile-img" />
+            <div className="col-auto p-sm-0 p-md-1 p-lg-2">
+                <img
+                    src={profilepicture}
+                    className="acc-profile-img img-fluid"
+                />
             </div>
-            <span className="col-10 p-2 acc-profile-username">{username}</span>
+            <span className="col-10 p-md-2 p-lg-3 acc-profile-username">
+                {username}
+            </span>
         </NavLink>
     )
 }
+
+//  py-md-2 py-lg-3
+// p-sm-2 p-md-3 p-lg-4
+
+// export default function AccountItem({
+//     profilepicture,
+//     username,
+//     handleClick,
+//     id,
+// }) {
+//     return (
+//         <NavLink
+//             onClick={() => handleClick(id)}
+//             to={`profile/${id}`}
+//             style={{ textDecoration: 'none', color: 'black' }}
+//             className="row py-1 align-items-center border-bottom border-bottom-primary account-item"
+//         >
+//             <div className="col-auto p-0">
+//                 <img src={profilepicture} className="acc-profile-img" />
+//             </div>
+//             <span className="col-10 p-2 acc-profile-username">{username}</span>
+//         </NavLink>
+//     )
+// }
--- panorabit/src/components/Home/Accounts/Accounts.jsx
+++ panorabit/src/components/Home/Accounts/Accounts.jsx
@@ -6,15 +6,34 @@ import AccountList from './AccountList'
 
 export default function Accounts() {
     return (
-        <div className="container-md pb-3 position-absolute shadow-lg top-50 start-50 translate-middle centered-div">
-            <div className="row account-select p-4 text-center">
-                <p className="h5">Select an account</p>
-            </div>
-            <div className="row account-list-wrapper px-3 pe-1">
-                <div className="pt-1 justify-content-center account-list">
-                    <AccountList />
+        <div className="container-fluid">
+            <div className="row bg bg-primary">
+                <div className="col-sm-7 col-md-6 col-lg-5 pb-3 position-absolute shadow-lg top-50 start-50 translate-middle centered-div">
+                    <div className="row account-select p-sm-4 p-md-4 p-lg-5 p-xl-8 text-center">
+                        <p className="h5 select-account">Select an account</p>
+                    </div>
+                    <div className="row account-list-wrapper pe-1">
+                        <div className="pt-1 px-5 justify-content-center account-list">
+                            <AccountList />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
+// export default function Accounts() {
+//     return (
+//         <div className="container  pb-3 position-absolute shadow-lg top-50 start-50 translate-middle centered-div">
+//             <div className="row account-select p-4 text-center">
+//                 <p className="h5">Select an account</p>
+//             </div>
+//             <div className="row account-list-wrapper px-3 pe-1">
+//                 <div className="pt-1 justify-content-center account-list">
+//                     <AccountList />
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
